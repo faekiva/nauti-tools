1cee61ad41f20a58b4b045e3dccafb91ea718ffd
Nexidava <nexidava@gmail.com>
2019-07-20 18:26:50 -0700

    added support for in-place files and argparsing

--- substitute_tokens.py
+++ substitute_tokens.py
@@ -1,12 +1,38 @@
-def substitute(text: str, substitutions: dict[str, str]) -> str:
-    tokenizedsubs = {f"$({key})":value for key, value in substitutions}
+from typing import Dict
+import argparse
+
+
+def substitute(text: str, substitutions: Dict[str, str]) -> str:
+    tokenizedsubs = {f"$({key})": value for key, value in substitutions}
     
     for key, value in tokenizedsubs:
         text = text.replace(key, value)
     
     return text
 
-def substitute_file(infile: str, outfile: str, substitutions: dict[str, str]) -> None:
+
+def substitute_file(infile: str, outfile: str, substitutions: Dict[str, str]) -> None:
     with open(infile, 'r') as instream:
-        with open(outfile, 'w+') as outstream :
-            outstream.write(substitute(instream.read(), substitutions))
+        text = instream.read()
+
+    with open(outfile, 'w+') as outstream:
+        outstream.write(substitute(text, substitutions))
+
+
+def parse():
+    p = argparse.ArgumentParser(
+        prog="substitute_tokens.py",
+        description="a simple utility for performing variable substitutions"
+    )
+
+    p.add_argument("infile", help="path to file to perform replacement on")
+    p.add_argument("outfile", help="path to file to perform replacement on")
+    p.add_argument('-r', "--replace", help="key value pair to replace", nargs=2, action="append")
+
+    return p.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse()
+    subs = {k: v for k, v in args.replace}
+    substitute_file(args.infile, args.outfile, subs)
