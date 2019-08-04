130dd4ad1deece5024bf0b9e4e37cf7233477984
Curtis <curtishilgenberg@gmail.com>
2019-08-04 17:35:29 +0000

    added scripts in Dockerfile

--- release/build-and-run.ps1
+++ release/build-and-run.ps1
@@ -1,2 +1,2 @@
 sudo docker build -t strollingfellow/nauti-release:latest $psscriptroot
-sudo docker run -it strollingfellow/nauti-release:latest
+sudo docker run -it strollingfellow/nauti-release:latest /bin/bash
